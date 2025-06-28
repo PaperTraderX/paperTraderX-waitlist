76de13330b45c8670f5ed4d65b43d7b57cd2fd2a
whokartikgambhir <gambhirkartik6@gmail.com>
2025-06-28 10:03:19 +0530

    rough

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// This file lets you customize Tailwind (optional). You only need to touch it if you want to:
+// ✅ Add custom colors, fonts, or breakpoints
+// ✅ Extend Tailwind’s default theme
